bfc6971515135fd7d0f99892a03228eb3d266dcb
Олег Матшин <89175955650@mail.ru>
2022-06-15 20:09:57 +0300

    New functions flatten_output, listcompr_output, iterutil_flatten and other changes of code.

--- HomeTask_Iter_Gener_150622.py
+++ HomeTask_Iter_Gener_150622.py
@@ -1,6 +1,7 @@
 import itertools
 import numpy
 from pandas.core.common import flatten
+from iteration_utilities import deepflatten
 
 nested_list_1 = [
 	['a', 'b', 'c'],
@@ -12,28 +13,59 @@ nested_list_2 = [
 	['d', 'e', 'f', 'h', False],
 	[1, 2, [[None, 3], [4 , 5, [6, 7]]]],
 ]
-
-def flatten_pandas():
-    flatten_list = list(flatten(nested_list_2))
-    print(flatten_list)
-    return flatten_list
-
-def flatten_itertools():
-    flatten_list = list(itertools.chain(*nested_list_1))
+# output function
+def flatten_output(flatten_list):
     for item_list in flatten_list:
         print(item_list)
     print(flatten_list)
-    print([sub_item for item in nested_list_1 for sub_item in item])
+
+#  list comprehensions function
+def listcompr_output(_list):
+    return [sub_item for item in _list for sub_item in item]
+
+# variant number 1 - deep flatten
+def iterutil_flatten(nested_list=nested_list_2):
+    flatten_list = list(deepflatten(nested_list))
+    flatten_output(flatten_list)
+    return  flatten_list
+
+# variant number 2 - deep flatten
+def flatten_pandas(nested_list=nested_list_2):
+    flatten_list = list(flatten(nested_list))
+    flatten_output(flatten_list)
     return flatten_list
 
-def flatten_numpy():
+# variant number 3 - 2d-flatten
+def flatten_itertools(nested_list=nested_list_1):
+    flatten_list = list(itertools.chain(*nested_list_1))
+    flatten_output(flatten_list)
+    print('List comprehension:', listcompr_output(nested_list))
+    return flatten_list
+
+# variant number 4 - 2d-flatten
+def flatten_numpy(nested_list=nested_list_1):
     flatten_list = list(numpy.concatenate(nested_list_1).flat)
-    print(flatten_list)
+    flatten_output(flatten_list)
+    print('List comprehension:', listcompr_output(nested_list))
     return flatten_list
 
+# variant number 5 - 2d-flatten
+def simple_flatten(nested_list=nested_list_1):
+    flatten_list = []
+    for item_list in nested_list:
+        if type(item_list) == list:
+            for subitem_list in item_list:
+                flatten_list.append(subitem_list)
+        else:
+            flatten_list.append(item_list)
+    flatten_output(flatten_list)
+    print('List comprehension:', listcompr_output(nested_list))
+    return flatten_list
 
 
 if __name__ == '__main__':
-    flatten_pandas()
+    iterutil_flatten()
+    # flatten_pandas()
     # flatten_itertools()
     # flatten_numpy()
+    # simple_flatten()
